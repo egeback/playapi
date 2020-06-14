52ccdc5f4ec5abe17d607af53c9d0cbfc9a8874b
Marky Egebäck <marky@egeback.se>
2020-06-15 00:21:17 +0200

    Update

--- internal/api/go.mod
+++ internal/api/go.mod
@@ -3,10 +3,6 @@ module github.com/egeback/play_media_api/internal/api
 go 1.14
 
 require (
-	github.com/egeback/play_media_api/internal/models v0.0.0-20200614215018-e24c01e204b6
+	github.com/egeback/play_media_api/internal/models v0.0.0-20200614215018-e24c01e204b6 // indirect
 	github.com/egeback/play_media_api/internal/parsers v0.0.0-20200614215018-e24c01e204b6 // indirect
-	github.com/gin-gonic/gin v1.6.3
-	github.com/hashicorp/go-version v1.2.0
-	github.com/jasonlvhit/gocron v0.0.0-20200423141508-ab84337f7963
-	github.com/liip/sheriff v0.0.0-20190308094614-91aa83a45a3d
 )
--- internal/main.go
+++ internal/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 
+	"github.com/egeback/play_media_api/internal/api"
 	"github.com/egeback/play_media_api/internal/parsers"
 	"github.com/egeback/play_media_api/internal/parsers/svtplay"
+	"github.com/egeback/play_media_api/internal/parsers/tv4play"
 )
 
 var (
@@ -16,7 +18,7 @@ var (
 
 func main() {
 	fmt.Println("Running Play Media API version: %s (%s)", BuildVersion, BuildTime)
-	parsers := []parsers.ParserInterface{new(svtplay.Parser), new(tv4play.parser)}
+	parsers := []parsers.ParserInterface{new(svtplay.Parser), new(tv4play.Parser)}
 	//parsers := []parsers.ParserInterface{new(tv4play.Parser)}
 
 	api := api.Init(parsers)
