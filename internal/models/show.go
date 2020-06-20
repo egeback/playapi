479c5ccc813ab14a8e90ab2959b97c9d31bb87e6
Marky Egebäck <marky@egeback.se>
2020-06-20 12:57:51 +0200

    Added ShowsAll to Show

--- internal/models/show.go
+++ internal/models/show.go
@@ -13,3 +13,19 @@ type Show struct {
 	Genre       string   `json:"genre" groups:"api"`
 	//Data interface
 }
+
+var shows = make([]Show, 0)
+
+//ShowsAll return shows with query
+func ShowsAll(q string) ([]Show, error) {
+	if q == "" {
+		return shows, nil
+	}
+	as := []Show{}
+	for k, s := range shows {
+		if q == s.Slug {
+			as = append(as, shows[k])
+		}
+	}
+	return as, nil
+}
