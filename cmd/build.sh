e5fdb2c1f9a77f4e6679cb881d5e52c7f96e65e4
Marky Egebäck <marky@egeback.se>
2020-06-29 01:30:13 +0200

    Update build.sh swag command

--- cmd/build.sh
+++ cmd/build.sh
@@ -2,5 +2,5 @@
 #version=0.1.3
 version=`cat VERSION`
 time=$(date)
-swag init --output ./internal/docs --parseInternal --exclude ./internal/models --generalInfo ./cmd/playapi/main.go
+swag init --output ./internal/docs --parseInternal --generalInfo ./cmd/playapi/main.go
 go build -o playapi -ldflags="-X 'github.com/egeback/playapi/internal/version.BuildTime=$time' -X 'github.com/egeback/playapi/internal/version.BuildVersion=$version'" ./cmd/playapi/main.go
